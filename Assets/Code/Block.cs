5797e34cd14b39a1a90ee8a32e24420416c071eb
Andrew's Macbook Air <andrewfriedman01@gmail.com>
2018-10-08 00:02:32 -0400

    Added App
    This is the first version that GitHub will see of Tree Climber!

---
+++ Assets/Code/Block.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+	GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<SpriteRenderer>().color = new Color32((byte)Random.Range(0,251), (byte)Random.Range(0,251), (byte)Random.Range(0,251), (byte)250);
+		Player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Player.transform.position.y - 1 > this.transform.position.y) {
+			Destroy(this.gameObject);
+		}
+	}
+}
